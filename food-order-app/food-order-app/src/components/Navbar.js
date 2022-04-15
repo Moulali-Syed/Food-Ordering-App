6c5f9d92e2be0260641f18e8a67ef41fe7bb4ad3
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-15 11:03:46 +0530

    making navbar responsive

--- food-order-app/food-order-app/src/components/Navbar.js
+++ food-order-app/food-order-app/src/components/Navbar.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.png';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const toggleIcon = () => {
+    setShowLinks(!showLinks);
+  };
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={showLinks ? 'open' : 'close'}>
         <img src={logo} alt="logo" />
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/">Menu</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/">Menu</Link>
         <Link to="/">About</Link>
         <Link to="/">Contact</Link>
+        <button onClick={toggleIcon}>
+          <MenuIcon />
+        </button>
       </div>
     </div>
   );
