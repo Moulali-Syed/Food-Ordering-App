b24acf51e5981d54f68f71cf3e604f51dc865194
Moulali Syed <syedmoulali1997@gmail.com>
2022-04-15 10:47:34 +0530

    styling navbar

--- food-order-app/food-order-app/src/components/Navbar.js
+++ food-order-app/food-order-app/src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="leftSide">
         <img src={logo} alt="logo" />
       </div>
-      <div className="rightSide">RightSide</div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/">Menu</Link>
+        <Link to="/">About</Link>
+        <Link to="/">Contact</Link>
+      </div>
     </div>
   );
 };
